b78f926ca556652f10769843e7340b2e227c36f2
akita11 <akita@ifdl.jp>
2024-03-01 15:24:42 +0900

    initial commit

--- ATOMS3_FW/src/main.cpp
+++ ATOMS3_FW/src/main.cpp
@@ -4,20 +4,32 @@
 
 #define I2C_ADDR_SDP810 0x25
 
+#define NX 128
+uint8_t p = 0;
+uint8_t val[NX];
+uint8_t modeS = 1;
+#define N_SPEED 3
+uint8_t wait[] = { 5, 20, 100 };
+uint8_t v0 = 64;
+
+#define N_MAG 3
+uint8_t modeM = 0;
+float mags[] = {10, 100, 1000};
+uint16_t color;
+uint16_t colors[] = { WHITE, GREEN, CYAN };
+
 // LCD: 128x128
 void setup() {
   auto cfg = M5.config();
   M5.begin(cfg);
   M5.Lcd.clear();
-  Serial.begin(115200);
-  Serial2.begin(115200, 2, 1); // RX/TX (Grove)
-/*
+//  Serial.begin(115200);
+  Serial.begin(115200, SERIAL_8N1, 2, 1); // RX/TX (Grove)
+//  Serial2.begin(115200, 2, 1); // RX/TX (Grove)
   Wire.begin(38, 39); // SDA, SCL
   Wire.beginTransmission(I2C_ADDR_SDP810);
   Wire.write(0x36); Wire.write(0x1e); // diff. pres. continuous, no average
   Wire.endTransmission();
-
-*/
 }
 
 // 2's complement
@@ -27,55 +39,48 @@ int conv_2s(uint16_t d)
   else return(d);
 }
 
-#define NX 128
-uint8_t p = 0;
-uint8_t val[NX];
-uint8_t mode = 2;
-#define N_MODE 3
-uint8_t wait[] = { 1, 10, 100 };
-uint8_t v0 = 64;
-uint16_t colors[] = { RED, GREEN, WHITE};
-uint16_t color = WHITE;
-
 void loop() {
   uint8_t px = p;
-/*
-  int vn = v0 + random(-5, 5);
-  if (vn < 0) vn = 0;
-  if (vn > 127) vn = 127;
-  val[p] = vn;
-  v0 = vn;
-  */
   uint8_t dp = 0;
   uint8_t dat[16];
  
-  Wire.requestFrom(I2C_ADDR_SDP810, 9);
-  while(Wire.available() && dp < 9) dat[dp++] = Wire.read();
+//  Wire.requestFrom(I2C_ADDR_SDP810, 9);
+//  while(Wire.available() && dp < 9) dat[dp++] = Wire.read();
+  Wire.requestFrom(I2C_ADDR_SDP810, 3);
+  while(Wire.available() && dp < 3) dat[dp++] = Wire.read();
  
   uint16_t DPb, Tempb, SFb;
   DPb   = dat[0] << 8 | dat[1];
-  Tempb = dat[3] << 8 | dat[4];
-  SFb   = dat[6] << 8 | dat[7];
+//  Tempb = dat[3] << 8 | dat[4];
+//  SFb   = dat[6] << 8 | dat[7];
 //  Serial.print(DPb, HEX); Serial.print(" ");
 //  Serial.print(Tempb, HEX); Serial.print(" ");
 //  Serial.print(SFb, HEX); Serial.print(" | ");
   int DPi, Tempi, SFi;
   DPi   = conv_2s(DPb);
-  Tempi = conv_2s(Tempb);
-  SFi   = conv_2s(SFb);
+//  Tempi = conv_2s(Tempb);
+//  SFi   = conv_2s(SFb);
 //  Serial.print(DPi); Serial.print(" ");
 //  Serial.print(Tempi); Serial.print(" ");
 //  Serial.print(SFi); Serial.print(" | ");
   float DP, Temp;
   DP = (float)DPi / 240.0;
-  Temp = (float)Tempi / 200.0;
+//  Temp = (float)Tempi / 200.0;
 //  Serial.print(t++); Serial.print(" ");
-  Serial.print(DP); Serial.print(" ");
+//  Serial.print(DP); Serial.print(" ");
 //  Serial.print(Temp); Serial.println("");
-  int vn = (uint8_t)(DP * 10.0) + 64;
+
+  int vn = (int)(DP * mags[modeM]) + 64;
   if (vn < 0) vn = 0;
   if (vn > 127) vn = 127;
+//  printf("%d %f %f\n", vn, mags[modeM], DP); 
+//  Serial.printf("%04x\n", (DPi & 0xffff));
+  Serial.printf("%d\n", (DPi & 0xffff));
+//  Serial.printf("%f\n", DP);
+//  Serial.printf("%d %f\n", vn, DP);
+//  Serial.printf("%c%c%c%c", 0x55, 0xaa, dat[0], dat[1]);
   val[px] = vn;
+  color = colors[modeM];
   for (uint8_t x = 0; x < NX; x++){
     M5.Lcd.drawFastVLine(x, 0, 128, BLACK);
     M5.Lcd.drawPixel(x, val[px], color);
@@ -83,11 +88,12 @@ void loop() {
   }
   p = (p + 1) % NX;
   M5.update();
-  if (M5.BtnA.wasClicked()){
-    mode = (mode + 1) % N_MODE;
-    color = colors[mode];
-//    M5.Lcd.setCursor(0, 0);
-//    M5.Lcd.printf("%d [Hz]", 1000 / wait[mode]);
+  if (M5.BtnA.wasDoubleClicked()){
+    modeS = (modeS + 1) % N_SPEED;
   }
-  delay(wait[mode]);
+  if (M5.BtnA.wasSingleClicked()){
+    modeM = (modeM + 1) % N_MAG;
+  }
+  delay(wait[modeS]);
 }
+
--- README.md
+++ README.md
@@ -1,4 +1,40 @@
-Get data from Differential Pressure Sensor, SDP810-125Pa, draw its graph, and send it via Grove's UART, using ATOMS3.
+# DiffPressute Unit
 
-Usage: https://twitter.com/akita11/status/1758277269104336898
+<img src="https://github.com/akita11/DiffPressureUNIT/blob/main/DiffPressureUNIT.jpg" width="320px">
+
+空気の動きも検出できるSensirion社の高感度差圧センサ[SDP810-125Pa](https://www.sensirion.com/jp/products/product-catalog/SDP810-125Pa/)で2つの口の差圧を計測し、Groveポートから計測値を送出します。計測値は搭載するATOM S3上のディスプレイで確認できます。計測されたデータは、Core2等で利用できます。（[使用例](https://twitter.com/akita11/status/1758277269104336898)）
+
+## 設定・操作
+
+ATOM S3のボタン（ディスプレイ面）で以下のように設定を変更できます
+
+- シングルクリック: 画面に表示されるグラフの感度（測定値の10倍（初期値：白）→100倍（緑）→1000倍（水色）→戻る）
+- ダブルクリック: データ送出周波数（20ms(50Hz)→100ms(10Hz)→5ms(200Hz)→戻る）※グラフの動く速さが変わります
+
+
+## UIFlowでの使い方
+
+<img src="https://github.com/akita11/DiffPressureUNIT/blob/main/UIFlow_sample.png" width="320px">
+
+UIFlow用のカスタムブロックを用意してあります（現時点ではCore2専用ですがピン割当を変更することで他のCore Basic等でも利用可能）。
+
+- UIFlowの"Custom"の"Open *.m5b"から、UIFlow/DiffPressureUNIT.m5b を開く→Custom内に"DiffPressureUnit"が追加される
+- UIFlow/DiffPressureUNIT_sample.m5f"を参考に利用できます
+- （得られる数値と実際の圧力との関係は後述）
+
+
+## UART通信プロトコル
+
+- UARTプロトコル: 115200bps, N81
+- データ形式: 計測値（-32768〜+32767:符号付き16bit）の文字列＋LF('\n')
+
+この「計測値」は、SDP810-125Paから出力される計測値です。実際の圧力への換算式はデータシートにあり、「計測値 ÷ 240 [Pa]」となります（例：計測値で"240"が1Pa）。なおSDP810-125Paのデータシートによれば、ゼロ点精度は0.08Pa、ゼロ点再現性は0.04Paです。
+
+## Author
+
+Junichi Akita (akita@ifdl.jp, @akita11)
+
+
+
+
 
